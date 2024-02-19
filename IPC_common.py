0a225105a46b441bf10d0ebe4bb07be1bc9da8b3
Spavid04 <Spavid04@users.noreply.github.com>
2024-02-19 18:01:27 +0200

    WIP something

--- IPC_common.py
+++ IPC_common.py
@@ -2,7 +2,7 @@ import abc
 import concurrent.futures
 import dataclasses
 import enum
-from typing import Generic, Optional, TypeVar
+from typing import Any, Callable, Generic, Optional, TypeVar
 
 
 CFuture = concurrent.futures.Future
@@ -33,21 +33,64 @@ def IPCFailed(reason: FailureReason) -> IPCResult:
 def IPCAborted() -> IPCResult:
     return IPCResult(False, FailureReason.Aborted, None)
 
+class FutureResult(abc.ABC, Generic[T]):
+    @abc.abstractmethod
+    def wait(self, timeout: float = None): ...
+
+    @abc.abstractmethod
+    def result(self, timeout: float = None) -> T: ...
+
+    @abc.abstractmethod
+    def abort(self) -> bool: ...
+
+    @abc.abstractmethod
+    def continueWith(self, func: Callable[["FutureResult", Any], None], context: Any = None): ...
+
+    @property
+    @abc.abstractmethod
+    def running(self) -> bool: ...
+    @property
+    @abc.abstractmethod
+    def aborted(self) -> bool: ...
+    @property
+    @abc.abstractmethod
+    def finished(self) -> bool: ...
+    @property
+    @abc.abstractmethod
+    def exception(self) -> Optional[Exception]: ...
+
 class NamedPipe(abc.ABC):
+    @abc.abstractmethod
     def __init__(self, name: str):
         self.Name = name
         self.Opened = False
 
-    def Create(self) -> IPCResult[bool]: pass
-    def Connect(self) -> CFuture[IPCResult]: pass
-    def Disconnect(self): pass
-    def Close(self): pass
+    @abc.abstractmethod
+    def Create(self) -> IPCResult[bool]: ...
 
-    def Read(self) -> CFuture[IPCResult[bytes]]: pass
-    def Write(self, data: bytes) -> CFuture[IPCResult]: pass
-    def Peek(self) -> CFuture[IPCResult[bool]]: pass
+    @abc.abstractmethod
+    def Connect(self) -> CFuture[IPCResult]: ...
 
-    def AbortPendingOperations(self): pass
+    @abc.abstractmethod
+    def Disconnect(self): ...
 
-    def __enter__(self) -> ("NamedPipe", CFuture[IPCResult[bool]]): pass
-    def __exit__(self, exc_type, exc_val, exc_tb): pass
+    @abc.abstractmethod
+    def Close(self): ...
+
+    @abc.abstractmethod
+    def Read(self) -> CFuture[IPCResult[bytes]]: ...
+
+    @abc.abstractmethod
+    def Write(self, data: bytes) -> CFuture[IPCResult]: ...
+
+    @abc.abstractmethod
+    def Peek(self) -> CFuture[IPCResult[bool]]: ...
+
+    @abc.abstractmethod
+    def AbortPendingOperations(self): ...
+
+    @abc.abstractmethod
+    def __enter__(self) -> ("NamedPipe", CFuture[IPCResult[bool]]): ...
+
+    @abc.abstractmethod
+    def __exit__(self, exc_type, exc_val, exc_tb): ...
